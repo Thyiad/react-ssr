dce3afa604aad847b575eeb53befaeed65db5683
Thyiad <1520583107@qq.com>
2020-07-17 18:39:38 +0800

    add config

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,12 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import routes from './route';
 import RouteWithSubRoutes from './components/RouteWithSubRoutes';
 import { Provider } from './redux/store';
+import 'antd/dist/antd.css';
 import '@/assets/scss/common.scss';
 import './app.scss';
 
+/** 暂时只考虑一级路由 */
 const App: FC = () => {
-    console.log('in app.tsx render..........');
     return (
         <Provider>
             <BrowserRouter>
--- webpack/config.js
+++ webpack/config.js
@@ -10,7 +10,7 @@ const CaseSensitivePathPlugin = require('case-sensitive-paths-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
-console.log('env is: '+process.env.NODE_ENV);
+console.log('env is: ' + process.env.NODE_ENV);
 
 const plugins = [
     new webpack.ProgressPlugin(),
@@ -25,22 +25,23 @@ const plugins = [
         filename: 'index.html',
     }),
 ];
-if(isDev){
+if (isDev) {
     plugins.push(new webpack.HotModuleReplacementPlugin());
     plugins.push(new webpack.NamedModulesPlugin());
-}else{
-    plugins.push(new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash].css',
-        chunkFilename: 'chunks/[id].[contenthash].css',
-    }));
+} else {
+    plugins.push(
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash].css',
+            chunkFilename: 'chunks/[id].[contenthash].css',
+        }),
+    );
 }
-
 
 module.exports = {
     entry: path.resolve(cwd, 'src/app'),
     output: {
         path: path.resolve(cwd, 'dist'),
-        filename: isDev ? 'js/[name].[hash].js': 'js/[name].[contentHash].js',
+        filename: isDev ? 'js/[name].[hash].js' : 'js/[name].[contentHash].js',
         chunkFilename: isDev ? 'chunks/[name].[hash].js' : 'chunks/[name].[contentHash].js',
         publicPath: '/',
     },
@@ -48,28 +49,30 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.scss', '.js', '.jsx', '.sass'],
         alias: {
-            "@": path.resolve(cwd, 'src'),
-        }
+            '@': path.resolve(cwd, 'src'),
+        },
     },
     module: {
         rules: moduleRules(),
     },
     plugins,
     watch: isDev,
-    devServer: isDev ? {
-        logLevel: 'warn', //不想看到那个complied successfully
-        contentBase: path.resolve(cwd, 'src'),
-        historyApiFallback: true,
-        compress: true,
-        host: envConfig.host,
-        port: envConfig.port,
-        hot: true,
-        open: true,
-        watchOptions: {
-            ignored: /node_modules/,    // 监听过多文件会占用cpu、内存，so，可以忽略掉部分文件
-            aggregateTimeout: 200,  // 默认200，文件变更后延时多久rebuild
-            poll: false,    // 默认false，如果不采用watch，那么可以采用poll（轮询）
-        },
-    } : undefined,
-    devtool: isDev ? "inline-source-map": undefined,
+    devServer: isDev
+        ? {
+              logLevel: 'warn', //不想看到那个complied successfully
+              contentBase: path.resolve(cwd, 'src'),
+              historyApiFallback: true,
+              compress: true,
+              host: envConfig.host,
+              port: envConfig.port,
+              hot: true,
+              open: true,
+              watchOptions: {
+                  ignored: /node_modules/, // 监听过多文件会占用cpu、内存，so，可以忽略掉部分文件
+                  aggregateTimeout: 200, // 默认200，文件变更后延时多久rebuild
+                  poll: false, // 默认false，如果不采用watch，那么可以采用poll（轮询）
+              },
+          }
+        : undefined,
+    devtool: isDev ? 'inline-source-map' : undefined,
 };
