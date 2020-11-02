b6746f07278e74061d2daa021db1cc4f7aecd77e
Thyiad <1520583107@qq.com>
2020-11-02 15:36:10 +0800

    feat: ignore za eslint, add env-config.local.js

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
+/* eslint-disable */
 const { pathToRegexp, match, parse, compile } = require('path-to-regexp');
+const envConfig = require('./webpack/./env-config');
 
-const keys = [];
-// const regexp = pathToRegexp('/foo/:bar', keys);
-const regexp = pathToRegexp('.*', keys);
-console.log(regexp);
-console.log(keys);
+console.log(JSON.stringify(envConfig, null, '    '));
+
+// const keys = [];
+// // const regexp = pathToRegexp('/foo/:bar', keys);
+// const regexp = pathToRegexp('.*', keys);
+// console.log(regexp);
+// console.log(keys);
--- webpack/env-config.js
+++ webpack/env-config.js
@@ -1,3 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+let localEnvConfig = {};
+const localEnvConfigPath = path.join(__dirname, 'env-config.local.js');
+if (fs.existsSync(localEnvConfigPath)) {
+    localEnvConfig = require(localEnvConfigPath);
+}
+
+/**
+ * 注：不要修改该文件，如果要覆盖，请新建一个 env-config.local.js（已 ignore 掉）
+ * 以保证此文件合并代码不会冲突
+ */
 module.exports = {
     /** 开发时监听的host */
     host: 'localhost',
@@ -9,4 +22,5 @@ module.exports = {
     sysType: 'ssr',
     /** 是否正在打包dll，暂时不启用dllPlugin和DllReferencePlugin */
     isDll: false,
+    ...localEnvConfig,
 };
