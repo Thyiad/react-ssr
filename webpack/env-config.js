9ab8b614b8bb3df16cb5371d78d2b8a4fc3e71cd
Thyiad <1520583107@qq.com>
2020-11-02 17:57:41 +0800

    feat: update env-config

--- webpack/env-config.js
+++ webpack/env-config.js
@@ -1,26 +1,21 @@
-const fs = require('fs');
-const path = require('path');
-
-let localEnvConfig = {};
-const localEnvConfigPath = path.join(__dirname, 'env-config.local.js');
-if (fs.existsSync(localEnvConfigPath)) {
-    localEnvConfig = require(localEnvConfigPath);
-}
-
 /**
  * 注：不要修改该文件，如果要覆盖，请新建一个 env-config.local.js（已 ignore 掉）
  * 以保证此文件合并代码不会冲突
  */
+let localEnvConfig = {};
+try {
+    localEnvConfig = require('./env-config.local');
+} catch (error) {}
+
 module.exports = {
     /** 开发时监听的host */
-    host: 'localhost',
+    host: localEnvConfig.host || 'localhost',
     /** 开发时客户端端口 */
-    clientPort: 8088,
+    clientPort: localEnvConfig.clientPort || 8088,
     /** 开发时服务端端口 */
-    serverPort: 8089,
+    serverPort: localEnvConfig.serverPort || 8089,
     /** 系统类型: ssr | spa */
-    sysType: 'ssr',
+    sysType: localEnvConfig.sysType || 'ssr',
     /** 是否正在打包dll，暂时不启用dllPlugin和DllReferencePlugin */
-    isDll: false,
-    ...localEnvConfig,
+    isDll: localEnvConfig.isDll || false,
 };
