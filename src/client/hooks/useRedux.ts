d11bca45cc7853e6853f5447cdee85b23e7461b7
Thyiad <1520583107@qq.com>
2020-12-13 23:48:50 +0800

    feat: update store

--- src/client/hooks/useRedux.ts
+++ src/client/hooks/useRedux.ts
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { Store, StoreContext } from '../redux/store';
+import { IState } from '@/redux/data';
 
 export const useRedux = () => {
     const { state, dispatch, actions } = useContext<StoreContext>(Store);
@@ -9,4 +10,12 @@ export const useRedux = () => {
         dispatch,
         actions,
     };
+};
+
+type StateKey = keyof IState;
+
+export const useProp = (key: StateKey) => {
+    const { state } = useContext<StoreContext>(Store);
+
+    return state[key];
 };
