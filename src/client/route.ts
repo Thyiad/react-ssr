bb1a02f40147ca83ef3f41dd837b931988d6ae11
Thyiad <1520583107@qq.com>
2021-02-07 13:02:35 +0800

    feat: route

--- src/client/route.ts
+++ src/client/route.ts
@@ -5,7 +5,42 @@ const NotFound = loadable(() => import(/* webpackChunkName: "404" */ './pages/40
 const PageError = loadable(() => import(/* webpackChunkName: "500" */ './pages/500/index'));
 const Demo = loadable(() => import(/* webpackChunkName: "demo" */ './pages/demo/index'));
 
+/**
+ * 系统路由：403、404等等
+ */
+const sysPages: RouteProps[] = [
+    {
+        title: '404',
+        path: '/404',
+        name: '404',
+        exact: true,
+        component: NotFound,
+    },
+    {
+        title: '500',
+        path: '/500',
+        name: '500',
+        exact: true,
+        component: PageError,
+    },
+    {
+        title: '页面找不到了',
+        name: '404',
+        path: '/404',
+        exact: true,
+        component: NotFound,
+    },
+];
+
 const routes: RouteProps[] = [
+    ...sysPages,
+    {
+        title: '首页',
+        name: 'home',
+        path: '/',
+        exact: true,
+        redirect: '/demo',
+    },
     {
         title: '示例页面',
         name: 'demo',
@@ -27,6 +62,7 @@ const routes: RouteProps[] = [
         name: '404',
         path: '*',
         exact: true,
+        redirect: '/404',
         component: NotFound,
     },
 ];
