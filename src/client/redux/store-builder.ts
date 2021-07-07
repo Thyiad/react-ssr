c7f2a52ca37e5e11c7f786060922aa35c634003c
Thyiad <1520583107@qq.com>
2021-07-07 11:26:30 +0800

    feat: update store-builder

--- src/client/redux/store-builder.ts
+++ src/client/redux/store-builder.ts
@@ -27,9 +27,36 @@ export type ReducerActions = { [key in ReducerKinds]: typeof reducers[key]['acti
 // buildReducers
 let reducerActions: ReducerActions;
 let reducerHandle: IReducers;
+
 export const buildActionReducers = (
     dispatch: Dispatch<IAction>,
 ): { actions: ReducerActions; reducerHandle: IReducers } => {
+    const _dispatch = (actionResult: any) => {
+        const resultType = Object.prototype.toString.call(actionResult);
+        switch (resultType) {
+            // 普通对象
+            case '[object Object]':
+                dispatch(actionResult);
+                break;
+            // 普通函数、异步函数
+            case '[object Function]':
+            case '[object AsyncFunction]':
+                // 函数内自己dispatch
+                const funResult = actionResult(_dispatch);
+                // 返回对象外层dispatch
+                return _dispatch(funResult);
+                break;
+            // Promise
+            case '[object Promise]':
+                return actionResult.then((res) => {
+                    _dispatch(res);
+                });
+                break;
+            default:
+                break;
+        }
+    };
+
     Object.keys(reducers).forEach((kindKey) => {
         // @ts-ignore
         // 这一块ts提示不好搞，所以直接注掉了
@@ -37,8 +64,8 @@ export const buildActionReducers = (
         const currentActions: { [key: string]: any } = {};
         Object.keys(currentReducer['actions']).forEach((actionKey: string) => {
             currentActions[actionKey] = (...args: any[]) => {
-                const action = currentReducer['actions'][actionKey].apply(null, args);
-                dispatch(action);
+                const actionResult = currentReducer['actions'][actionKey].apply(null, args);
+                return _dispatch(actionResult);
             };
         });
         reducerActions = {
